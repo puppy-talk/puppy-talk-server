799afc0b3a95f8ecafd644dc932e8e119dcdae23
유건 <yukeon97@gmail.com>
2025-08-23 15:10:17 +0900

    refactor: ApiSuccessMessage에서 '성공적으로' 표현 제거
    - 모든 성공 메시지에서 '성공적으로'라는 표현 제거
    - 더 간결하고 자연스러운 메시지로 개선
    - 사용자 경험 향상을 위한 메시지 간소화

--- puppytalk-api/src/main/java/com/puppytalk/support/ApiSuccessMessage.java
+++ puppytalk-api/src/main/java/com/puppytalk/support/ApiSuccessMessage.java
@@ -6,20 +6,20 @@ package com.puppytalk.support;
 public enum ApiSuccessMessage {
     
     // Pet
-    PET_CREATE_SUCCESS("반려동물이 성공적으로 생성되었습니다"),
-    PET_LIST_SUCCESS("반려동물 목록을 성공적으로 조회했습니다"),
-    PET_DETAIL_SUCCESS("반려동물 정보를 성공적으로 조회했습니다"),
-    PET_DELETE_SUCCESS("반려동물이 성공적으로 삭제되었습니다"),
+    PET_CREATE_SUCCESS("반려동물이 생성되었습니다"),
+    PET_LIST_SUCCESS("반려동물 목록을 조회했습니다"),
+    PET_DETAIL_SUCCESS("반려동물 정보를 조회했습니다"),
+    PET_DELETE_SUCCESS("반려동물이 삭제되었습니다"),
     
     // Chat
-    CHAT_MESSAGE_SEND_SUCCESS("메시지가 성공적으로 전송되었습니다"),
-    CHAT_ROOM_CREATE_SUCCESS("채팅방이 성공적으로 생성되었습니다"),
-    CHAT_ROOM_FIND_SUCCESS("기존 채팅방을 성공적으로 조회했습니다"),
-    CHAT_ROOM_LIST_SUCCESS("채팅방 목록을 성공적으로 조회했습니다"),
-    CHAT_MESSAGE_LIST_SUCCESS("채팅 메시지 목록을 성공적으로 조회했습니다"),
+    CHAT_MESSAGE_SEND_SUCCESS("메시지가 전송되었습니다"),
+    CHAT_ROOM_CREATE_SUCCESS("채팅방이 생성되었습니다"),
+    CHAT_ROOM_FIND_SUCCESS("기존 채팅방을 조회했습니다"),
+    CHAT_ROOM_LIST_SUCCESS("채팅방 목록을 조회했습니다"),
+    CHAT_MESSAGE_LIST_SUCCESS("채팅 메시지 목록을 조회했습니다"),
     
     // Common
-    OPERATION_SUCCESS("작업이 성공적으로 완료되었습니다");
+    OPERATION_SUCCESS("작업이 완료되었습니다");
     
     private final String message;
     
